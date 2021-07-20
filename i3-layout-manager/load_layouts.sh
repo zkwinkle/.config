72325b41eba9bd0d428e09d678940cae34ec4b0e
Ignacio Vargas <ignaevc@gmail.com>
2021-07-20 17:00:46 -0600

    Marked the 4th workspace for coding and made it so load_layouts scripts accepts workspaces as variables so it knows about the symbols

--- i3-layout-manager/load_layouts.sh
+++ i3-layout-manager/load_layouts.sh
@@ -1,11 +1,11 @@
 # Messaging apps workspace
-i3 workspace 9
+i3 workspace $3
 $HOME/.config/i3-layout-manager/layout_manager.sh $HOME/.config/i3-layout-manager/layouts/layout-MESSAGING.json
 i3 exec discord
 i3 exec telegram-desktop
 
 ## Startup terminals
-i3 workspace 2
+i3 workspace $2
 $HOME/.config/i3-layout-manager/layout_manager.sh $HOME/.config/i3-layout-manager/layouts/layout-TERMINALS.json
 #It receives the default terminal as an argument
 i3 exec $1
