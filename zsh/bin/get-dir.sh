1cdb9d0c8a37ce30f036d8a7eba1210b93bf562d
Ignacio Vargas <ignaevc@gmail.com>
2021-08-27 15:12:24 -0600

    Removed trailing slash from path inside git projects

--- zsh/bin/get-dir.sh
+++ zsh/bin/get-dir.sh
@@ -4,5 +4,6 @@ dir="%B%F{blue}%3~"
 if [ "$(git rev-parse --is-inside-work-tree 2>/dev/null)" = "true" ]
 then
 	dir="%B%F{14}$(basename "$(git rev-parse --show-toplevel)")/$(git rev-parse --show-prefix)"
+	dir=${dir%?} # Remove the last character (trailing /)
 fi
 echo $dir
