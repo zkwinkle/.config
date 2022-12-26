cf1c33afbaa59079e9630583055aeb9cbd64207f
Ignacio Vargas <ignaevc@gmail.com>
2022-12-26 15:32:31 -0600

    Improve post-wal script to not have a hardcoded resolution it makes the i3lock image fit but adapt to the current monitor

--- zsh/bin/post-wal.sh
+++ zsh/bin/post-wal.sh
@@ -7,5 +7,10 @@ pywal-discord &
 
 # change i3lock img
 IMG=$(cat "${HOME}/.cache/wal/last_used_theme" | grep -Eo "_.*${EXTENSIONS}" | sed "s/_/\//g" | sed "s/\(.*\)\//\1./")
-# Need to convert to png, specifically 1920x1080 dims for my screen
-ffmpeg -y -i "${IMG}" -vf scale=-1:1080,crop=1920:1080 "${ZDOTDIR}/i3lock.png"
+
+# Need to convert to png with correct dimensions
+RES=$(xrandr --current | grep -o 'current [0-9]\+ x [0-9]\+' | grep -o '[0-9]\+ x [0-9]\+')
+HOR=${RES% x*}
+VER=${RES#*x }
+
+ffmpeg -y -i "${IMG}" -vf scale=-1:"$VER",crop="$HOR":"$VER" "${ZDOTDIR}/i3lock.png"
