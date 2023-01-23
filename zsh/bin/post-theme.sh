6fa0fe399cbb8f7503a0a7cb633c761c5eff7d0d
Ignacio Vargas <ignaevc@gmail.com>
2023-01-23 09:02:33 -0600

    Fix shellcheck warnings and generate i3lock for images that have different ratios

--- zsh/bin/post-theme.sh
+++ zsh/bin/post-theme.sh
@@ -8,6 +8,10 @@ RES=$(xrandr --current | grep -o 'current [0-9]\+ x [0-9]\+' | grep -o '[0-9]\+ 
 HOR=${RES% x*}
 VER=${RES#*x }
 
-ffmpeg -y -i "$IMG" -vf scale=-1:"$VER",crop="$HOR":"$VER" "${ZDOTDIR}/i3lock.png" &> /dev/null
+ffmpeg -y -i "$IMG" -vf scale=-1:"$VER",crop="$HOR":"$VER" "${ZDOTDIR}/i3lock.png" 2> /dev/null
+
+if [ $? ]; then
+	ffmpeg -y -i "$IMG" -vf scale="$HOR":-1,crop="$HOR":"$VER" "${ZDOTDIR}/i3lock.png" 2> /dev/null
+fi
 
 feh --bg-fill "$IMG"
