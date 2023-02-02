b1cf639f523d80f0434544c62bba65d9b7dd7217
Ignacio Vargas <ignaevc@gmail.com>
2023-02-02 16:11:16 -0600

    Fix ':Recolor' command not being able to find the base16.lua file

--- nvim/lua/core/commands.lua
+++ nvim/lua/core/commands.lua
@@ -1,6 +1,6 @@
 local new_command = vim.api.nvim_create_user_command
 
 new_command("Recolor", function()
-	vim.api.nvim_command("source " .. package.searchpath("plugins.configs.base16", package.path))
+	vim.api.nvim_command("source $XDG_CONFIG_HOME/nvim/lua/plugins/configs/base16.lua")
 	require("plugins.configs.others").lualine()
 end, {})
