1b879813faa551bcfa4b466b711c8fc3f84f7dbc
Ignacio Vargas <ignaevc@gmail.com>
2023-12-09 03:40:15 -0600

    Move lazy=false and priority=1000 above the config line for nvim-base16
    This was done with the hopes that it could avoid nvim using weird colors
    when a plugin requires installation.

--- nvim/lua/plugins/configs/editor/nvim-base16.lua
+++ nvim/lua/plugins/configs/editor/nvim-base16.lua
@@ -1,9 +1,9 @@
 return {
 	dir = "~/.config/nvim/lua/plugins/nvim-base16",
 	name = "nvim-base16",
+	lazy = false,
+	priority = 1000,
 	config = function()
 		require("plugins.nvim-base16.colors")
 	end,
-	lazy = false,
-	priority = 1000,
 }
