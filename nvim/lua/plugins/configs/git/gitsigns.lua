4beb34316f6488012252860a6ca0d937b869a33b
Ignacio Vargas <ignaevc@gmail.com>
2023-12-08 20:59:10 -0600

    Fix gitsigns not loading

--- nvim/lua/plugins/configs/git/gitsigns.lua
+++ nvim/lua/plugins/configs/git/gitsigns.lua
@@ -4,5 +4,6 @@ local mappings = require('core.mappings').gitsigns
 return {
 	"lewis6991/gitsigns.nvim",
 	event = "VeryLazy",
+	opts = {}, -- necessary for lazy to call the setup()
 	keys = utils.mapping_to_lazy_keys(mappings, { noremap = true }),
 }
