1522a3676f63890a8e7530e3b82843a732fabe44
Ignacio Vargas <ignaevc@gmail.com>
2024-04-30 00:16:44 -0600

    Improve cmp config

--- nvim/lua/plugins/configs/code/cmp.lua
+++ nvim/lua/plugins/configs/code/cmp.lua
@@ -52,6 +52,7 @@ return {
 		opts = function()
 			vim.api.nvim_set_hl(0, "CmpGhostText", { link = "Comment", default = true })
 			local cmp = require("cmp")
+			local luasnip = require("luasnip")
 			local defaults = require("cmp.config.default")()
 			return {
 				completion = {
@@ -67,17 +68,19 @@ return {
 				},
 				snippet = {
 					expand = function(args)
-						require("luasnip").lsp_expand(args.body)
+						luasnip.lsp_expand(args.body)
 					end,
 				},
 				mapping = cmp.mapping.preset.insert({
-					["<C-n>"] = cmp.mapping(function(fallback)
+					["<Tab>"] = cmp.mapping(function(fallback)
 						if cmp.visible() then
 							if #cmp.get_entries() == 1 then
 								cmp.confirm({ select = true })
 							else
 								cmp.select_next_item()
 							end
+						elseif luasnip.expand_or_jumpable() then
+							luasnip.expand_or_jump()
 						elseif has_words_before() then
 							cmp.complete()
 							if #cmp.get_entries() == 1 then
@@ -87,12 +90,30 @@ return {
 							fallback()
 						end
 					end, { "i", "s" }),
-					["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
-					["<C-d>"] = cmp.mapping.scroll_docs(-4),
-					["<C-u>"] = cmp.mapping.scroll_docs(4),
-					["<C-Space>"] = cmp.mapping.complete(),
+					["<S-Tab>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_prev_item()
+						elseif luasnip.jumpable(-1) then
+							luasnip.jump(-1)
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
+					["<C-n>"] = cmp.mapping(function(fallback) fallback() end),
+					["<C-d>"] = cmp.mapping.scroll_docs(4),
+					["<C-u>"] = cmp.mapping.scroll_docs(-4),
 					["<C-e>"] = cmp.mapping.abort(),
-					["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+					["<CR>"] = cmp.mapping({
+						i = function(fallback)
+							if cmp.visible() and cmp.get_active_entry() then
+								cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
+							else
+								fallback()
+							end
+						end,
+						s = cmp.mapping.confirm({ select = true }),
+						c = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true }),
+					}),
 					["<S-CR>"] = cmp.mapping.confirm({
 						behavior = cmp.ConfirmBehavior.Replace,
 						select = true,
@@ -117,11 +138,12 @@ return {
 				sorting = defaults.sorting,
 			}
 		end,
-		---@param opts cmp.ConfigSchema
 		config = function(_, opts)
 			for _, source in ipairs(opts.sources) do
 				source.group_index = source.group_index or 1
 			end
+			opts.completion = { completeopt = "menu,menuone,noselect" };
+			opts.preselect = 'none';
 			require("cmp").setup(opts)
 		end,
 	}
