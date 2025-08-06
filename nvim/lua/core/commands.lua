bb87e27da6ca16b6c6e65f0075e60632e90f8213
Ignacio Vargas <ignaevc@gmail.com>
2025-08-06 15:03:29 -0600

    Refresh lualine through it's function to stop it from flickering after recoloring

--- nvim/lua/core/commands.lua
+++ nvim/lua/core/commands.lua
@@ -2,5 +2,5 @@ local new_command = vim.api.nvim_create_user_command
 
 new_command("Recolor", function()
 	vim.cmd("source $XDG_CONFIG_HOME/nvim/lua/plugins/nvim-base16/colors.lua")
-	vim.cmd("Lazy reload lualine.nvim")
+  require('lualine').refresh()
 end, {})
