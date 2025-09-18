44487e91e3be0ab2ad39a718b8f2e18806313e3b
Ignacio Vargas <ignaevc@gmail.com>
2025-09-18 12:32:50 -0600

    Add comment explaining vim-ledger

--- nvim/lua/plugins/configs/code/vim-ledger.lua
+++ nvim/lua/plugins/configs/code/vim-ledger.lua
@@ -1,4 +1,9 @@
 -- Get LSP-like support for (h)ledger files
+--
+-- Provides:
+-- - Syntax highlight
+-- - Auto-format
+-- - Auto-complete (of accounts with omni-complete ctrl-x ctrl-o)
 
 return {
 	"ledger/vim-ledger",
