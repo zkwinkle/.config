79400741f11f5c33ec19a44772e4b953d6c81b64
Ignacio Vargas <ignaevc@gmail.com>
2025-10-01 16:02:03 -0600

    Add low battery alert notification

--- dunst/dunst_alert.sh
+++ dunst/dunst_alert.sh
@@ -3,20 +3,28 @@
 SOUNDS="$XDG_CONFIG_HOME/dunst/alert_sounds"
 
 play_sound() {
+  if notification_already_present; then
+    mpv "$SOUNDS"/portal_button_positive.m4a --volume=20 &
+    return
+  fi
 
-	if [ "$DUNST_APP_NAME" = "Dunst Timer" ]; then
-		return
-	fi
+  if [ "$DUNST_APP_NAME" = "Battery Notification" ]; then
+    return
+  fi
 
-	if [ "$DUNST_APP_NAME" = "Spotify" ]; then
-		return
-	fi
+  if [ "$DUNST_APP_NAME" = "Dunst Timer" ]; then
+    return
+  fi
 
-	if [ "$DUNST_URGENCY" = "CRITICAL" ]; then
-		mpv "$SOUNDS"/portal_button_positive.m4a --volume=80 &
-	else
-		mpv "$SOUNDS"/facebook_chat_pop.mp3 --volume=50 &
-	fi
+  if [ "$DUNST_APP_NAME" = "Spotify" ]; then
+    return
+  fi
+
+  if [ "$DUNST_URGENCY" = "CRITICAL" ]; then
+    mpv "$SOUNDS"/portal_button_positive.m4a --volume=80 &
+  else
+    mpv "$SOUNDS"/facebook_chat_pop.mp3 --volume=50 &
+  fi
 
 }
 
---
+++ dunst/low_battery_alert.sh
@@ -0,0 +1,65 @@
+#!/bin/sh
+
+# Script runs permanently on a loop. Should maybe not do that?
+#
+# Started by systemd user file: [../systemd/user/alert-battery.service]
+
+# Set to the correct battery device.
+#
+# Look at devices on /sys/class/power_supply/
+# For example: /sys/class/power_supply/BAT1
+BATTERY_DEVICE=BAT1
+
+STATUS_FILE="/sys/class/power_supply/${BATTERY_DEVICE}/status"
+CAPACITY_FILE="/sys/class/power_supply/${BATTERY_DEVICE}/capacity"
+
+# $1 = current charge level (CAPACITY)
+# $2 = whether to play a sound, we want to play a sound only on the first time
+#      the notification shows up (so when the user first enters the low battery
+#      state)
+# $3 = the notification ID to use, we want to overwrite the same notification
+#      because changing the app name causes them not to stack
+send_notification() {
+  NAME="Battery Notification"
+  TITLE="⚠️🔌 Low Battery"
+  BODY="Charge is at ${1}%"
+  URGENCY="critical"
+
+
+  STACK_TAG="battery-notification"
+
+  if [ "$2" = "true" ]; then
+    # We change the app name in order to trigger sound, since the "Battery
+    # Notification" app name gets filtered out by ./dunst_alert.sh
+    NAME="$NAME (different app name to trigger sound)"
+  fi
+
+  if [ -z "$3" ]; then
+    notify-send "$TITLE" "$BODY" -a "$NAME" --hint=string:x-dunst-stack-tag:"$STACK_TAG" -p -u "$URGENCY"
+  else
+    notify-send "$TITLE" "$BODY" -a "$NAME" --hint=string:x-dunst-stack-tag:"$STACK_TAG" -p -u "$URGENCY" -r "$3"
+  fi
+}
+
+while true; do
+  STATUS=$(cat "$STATUS_FILE")
+  CAPACITY=$(cat "$CAPACITY_FILE")
+
+  if [ "$STATUS" = "Discharging" ] && [ "$CAPACITY" -lt 10 ]; then
+    if [ -z "$FIRST_TIME" ]; then
+      PLAY_SOUND="true"
+      FIRST_TIME="done"
+      NOTIFICATION_ID=
+    else
+      PLAY_SOUND=
+    fi
+
+    NOTIFICATION_ID=$(send_notification "$CAPACITY" "$PLAY_SOUND" "$NOTIFICATION_ID")
+
+  else
+    # Reset flag
+    FIRST_TIME=
+  fi
+
+  sleep 10
+done
